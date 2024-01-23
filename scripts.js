27d7ef5db79bf27b181f1b090b9392684cf888e9
iam_sravani <98469507+sravani-321@users.noreply.github.com>
2024-01-23 18:37:15 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,43 @@
-const title = document.getElementById('title');
-const nameField = document.getElementById('nameField');
-const signinBtn = document.getElementById('signinBtn');
-const signupBtn = document.getElementById('signupBtn');
+function solve() { 
+	let password = 
+		document.getElementById('password').value; 
+	let repassword = 
+		document.getElementById('repassword').value; 
+	let mobile = 
+		document.getElementById('mobile').value; 
+	let mail = 
+		document.getElementById('email').value; 
+	let flag = 1; 
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
 
-signupBtn.onclick = function(){
-    title.innerHTML = "Sign Up";
-    nameField.style.display = "block";
-}
-signinBtn.onclick = function(){
-    title.innerHTML = "Login";
-    nameField.style.display = "none";
+	if (!emailRegex.test(mail)) { 
+		flag = 0; 
+		pass.innerText = 
+			'Please enter a valid email address.'; 
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
+
+	if (password !== repassword) { 
+		flag = 0; 
+		pass.innerText = 
+			"Passwords do not match. Please re-enter."; 
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
+
+	let passwordRegex = 
+		/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])\S{8,}$/; 
+
+	if (!passwordRegex.test(password)) { 
+		flag = 0; 
+		pass.innerText = 'Please enter a valid mobile number.';
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
+	if (flag) 
+		alert("Form submitted"); 
 }
